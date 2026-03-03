9cff82a314690df9bc475b24e56a868ba3fc24d5
Moaaz Hassan <Moaaz.Ebrahim.Hassan@gmail.com>
2026-03-03 15:27:17 +0200

    Add ProtectedRoute and routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './components/layout/MainLayout'
+import ProtectedRoute from './components/common/ProtectedRoute/ProtectedRoute'
+import CheckoutPage1 from './pages/Checkout pages/CheckoutPage1'
+import CheckoutPage2 from './pages/Checkout pages/CheckoutPage2'
+import CheckoutPage3 from './pages/Checkout pages/CheckoutPage3'
 
-export default App;
+function App() {
+
+  const routers = createBrowserRouter([
+    {path : '' , element : <MainLayout/> , children : 
+      [
+      {path : "checkout-1" , element : <ProtectedRoute><CheckoutPage1 /></ProtectedRoute>},
+      { path: 'checkout-2', element: <ProtectedRoute><CheckoutPage2 /></ProtectedRoute> },
+      { path: 'checkout-3', element: <ProtectedRoute><CheckoutPage3 /></ProtectedRoute> },
+    ]
+    }
+      
+    ])
+    
+  return (
+    <div>
+      <RouterProvider router={routers}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
