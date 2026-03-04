c39ae6c1c61fdd091fedad1116290af7ec13a938
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-03-04 17:52:49 +0200

    Footer components

---
+++ src/components/layout/footer/FooterList.tsx
@@ -0,0 +1,22 @@
+const FooterList = ({
+  title,
+  items,
+  className = "",
+}: {
+  title: string;
+  items: string[];
+  className?: string;
+}) => {
+  return (
+    <div className={`text-gray-800 ${className}`}>
+      <h3 className="text-xl mb-5 text-gray-900">{title}</h3>
+      <ul className="text-sm flex-col space-y-4">
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FooterList;
