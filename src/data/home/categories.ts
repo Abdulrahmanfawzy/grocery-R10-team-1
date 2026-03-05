b37c988ce438a39528a5acde91e9ae39c719afff
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-03-05 21:33:55 +0200

    the data that Home component need

---
+++ src/data/home/categories.ts
@@ -0,0 +1,8 @@
+import type { Category } from "@/types/categories";
+
+export const categories: Category[] = [
+  { id: 2, name: "Vegetables" },
+  { id: 1, name: "Fruits" },
+  { id: 3, name: "Coffee & teas" },
+  { id: 4, name: "Meat" },
+];
