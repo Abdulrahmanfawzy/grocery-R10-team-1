22d743ba3f8f72c2e1ef37492a8ef31de1340d21
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-03-07 01:05:55 +0200

    feat: Implement core application routing with react-router-dom, defining main layouts and routes for home, product, checkout, cart, and profile sections.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import MainLayout from "./components/layout/MainLayout";
 import CheckoutPage1 from "./pages/Checkout pages/CheckoutPage1";
 import CheckoutPage2 from "./pages/Checkout pages/CheckoutPage2";
 import CheckoutPage3 from "./pages/Checkout pages/CheckoutPage3";
-<<<<<<< HEAD
 
-=======
->>>>>>> c8ff959cb7e9663e9796f660c7c58d7c75c236d1
 import Dashboard from "./pages/profile/Dashboard";
 import PersonalInfo from "./pages/profile/PersonalInfo";
 import OrderHistory from "./pages/profile/OrderHistory";
@@ -19,14 +16,10 @@ import Security from "./pages/profile/Security";
 import Support from "./pages/profile/Support";
 import Settings from "./pages/profile/Settings";
 import CategoryPage from "./pages/Category/CategoryPage";
-<<<<<<< HEAD
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
-=======
 import CartPage from "./pages/CartPage";
 import ProductList from "./product-list/productlist";
-import Home from "./pages/Home";
->>>>>>> c8ff959cb7e9663e9796f660c7c58d7c75c236d1
 
 function App() {
   const routers = createBrowserRouter([
@@ -43,13 +36,10 @@ function App() {
           element: <CategoryPage />,
         },
         {
-<<<<<<< HEAD
-=======
           path: "products",
           element: <ProductList />,
         },
         {
->>>>>>> c8ff959cb7e9663e9796f660c7c58d7c75c236d1
           path: "checkout-1",
           element: <CheckoutPage1 />,
         },
@@ -62,30 +52,26 @@ function App() {
           element: <CheckoutPage3 />,
         },
         {
-<<<<<<< HEAD
           path: "product-details",
           element: <ProductDetails />,
-=======
+        },
+        {
           path: "cart",
           element: <CartPage />,
->>>>>>> c8ff959cb7e9663e9796f660c7c58d7c75c236d1
         },
         {
           path: "profile",
           element: <ProfileLayout />,
           children: [
-<<<<<<< HEAD
             { index: true, element: <Dashboard /> },
             { path: "dashboard", element: <Dashboard /> },
             { path: "personal-info", element: <PersonalInfo /> },
             { path: "order-history", element: <OrderHistory /> },
             { path: "wallet", element: <Wallet /> },
-=======
             { path: "dashboard", element: <Dashboard /> },
             { path: "info", element: <PersonalInfo /> },
             { path: "orders-history", element: <OrderHistory /> },
             { path: "payments-wallet", element: <Wallet /> },
->>>>>>> c8ff959cb7e9663e9796f660c7c58d7c75c236d1
             { path: "smart-list", element: <SmartList /> },
             { path: "addresses", element: <Addresses /> },
             { path: "loyalty", element: <Loyalty /> },
