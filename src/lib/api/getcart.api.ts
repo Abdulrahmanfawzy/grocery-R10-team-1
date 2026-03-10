1581812879101e7b673ce76dfedd9d08bfc44388
mahmoudelnahas180 <mahmoudelnahas180@gmail.com>
2026-03-10 22:52:34 +0200

    feat: Implement `getCart` API function to fetch cart data.

--- src/lib/api/getcart.api.ts
+++ src/lib/api/getcart.api.ts
@@ -2,5 +2,6 @@ import axiosInstance from "../Axios/axiosInstance";
 
 export const getCart = async () => {
   const response = await axiosInstance.get("/api/cart");
+  console.log(response.data);
   return response.data;
 };
